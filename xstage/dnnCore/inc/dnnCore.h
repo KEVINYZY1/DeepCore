c3dc05b449371ac0ccb32eaa490095b1a6d03147
XiuYuLi <295553381@qq.com>
2016-06-18 14:21:55 +0800

    加入更快的卷积操作cellconv，同时进一步fftconv的效率也有进一步提升
    cellconv在数据尺寸>32时比fftconv更快，同时对数据尺寸没有限制

--- xstage/dnnCore/inc/dnnCore.h
+++ xstage/dnnCore/inc/dnnCore.h
@@ -17,93 +17,75 @@
 	#define __APICALL__
 #endif
 
-#ifndef uint64
-	#ifdef _MSC_VER
-		#define uint64 unsigned __int64
-	#else
-		#define uint64 unsigned long long
-	#endif
-#endif
-
+#include<stdint.h>
 #include<cuda.h>
 
-#define dnnPrecisionMaskFloat		0x0
-#define dnnPrecisionMaskMixed		0x1
-#define dnnPrecisionMaskHalf		0x2
+#define dnnPrecisionMaskFloat				0x0
+#define dnnPrecisionMaskMixed				0x1
+#define dnnPrecisionMaskHalf				0x2
 
-#define dnnTensorTypeMaskFilter		0x4
-#define dnnTensorTypeMaskBias		0x5
-#define dnnTensorTypeMaskFC			0x6
+#define dnnTensorTypeMaskFilter				0x4
+#define dnnTensorTypeMaskBias				0x5
+#define dnnTensorTypeMaskFullConnection		0x6
 
-#define dnnPoolingMaskAvg			0x0
-#define dnnPoolingMaskMax			0x4
+#define dnnPoolingMaskAvg					0x0
+#define dnnPoolingMaskMax					0x4
 
-#define	dnnActivationMaskReLU		0x0
-#define	dnnActivationMaskPReLU		0x4
-#define	dnnActivationMaskELU		0x5
-#define	dnnActivationMaskTanh		0x6
-#define	dnnActivationMaskSigmod		0x7
-
-#define dnnReductionMaskAdd			0x0
-#define dnnReductionMaskMax			0x4
-#define dnnReductionMaskMin			0x5
+#define	dnnActivationMaskReLU				0x0
+#define	dnnActivationMaskTanh				0x6
+#define	dnnActivationMaskSigmod				0x7
 
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
-typedef struct __dnnConvOp		*	dnnConvOp;
-typedef struct __dnnFFTConvOp	*	dnnFFTConvOp;
-typedef struct __dnnCellConvOp	*	dnnCellConvOp;
-typedef struct __dnnPoolingOp	*	dnnPoolingOp;
-typedef struct __dnnBiasOp		*	dnnBiasOp;
-typedef struct __dnnReductionOp	*	dnnReductionOp;
-typedef struct __dnnActivationOp*	dnnActivationOp;
+typedef struct __dnnConvOp			*	dnnConvOp;
+typedef struct __dnnFFTConvOp		*	dnnFFTConvOp;
+typedef struct __dnnCellConvOp		*	dnnCellConvOp;
+typedef struct __dnnPoolingOp		*	dnnPoolingOp;
+typedef struct __dnnBiasOp			*	dnnBiasOp;
+typedef struct __dnnActivationOp	*	dnnActivationOp;
+typedef struct __dnnFilterOp		*	dnnFilterOp;
 
 typedef enum dnnStatus{
-	dnnSuccess=0			,
-	dnnErrorInvalidValue	,
-	dnnErrorInvalidDevice	,
-	dnnErrorOutOfMaxSize	,
-	dnnErrorOutOfMemory		,
-	dnnErrorOutOfDeviceMemory
+	dnnSuccess=0				,
+	dnnErrorInvalidValue		,
+	dnnErrorInvalidDevice		,
+	dnnErrorOutOfMaxSize		,
+	dnnErrorOutOfMemory			,
+	dnnErrorOutOfDeviceMemory	
 } dnnStatus_t;
 
-__APIENTRY__	dnnStatus_t		__APICALL__ dnnInit();
-__APIENTRY__	int				__APICALL__ dnnSuperdeviceCount();
-__APIENTRY__	int				__APICALL__ dnnSuperdeviceSize( int );
-__APIENTRY__	int				__APICALL__ dnnOptimalSuperdeviceID();
-__APIENTRY__	void			__APICALL__ dnnSetDevice( int, int );
+__APIENTRY__	dnnStatus_t	__APICALL__ dnnInit();
+__APIENTRY__	dnnStatus_t	__APICALL__ dnnSetDevice( int );
 
-__APIENTRY__	uint64			__APICALL__ dnnCreateTensorShape( unsigned int, unsigned int, unsigned int, unsigned int, unsigned int );
-__APIENTRY__	size_t			__APICALL__ dnnTensorPitch( uint64 );
-__APIENTRY__	dnnStatus_t		__APICALL__ dnnCreateTensor( void**, uint64 );
+__APIENTRY__	uint64_t	__APICALL__ dnnCreateTensorShape( unsigned int, unsigned int, unsigned int, unsigned int, unsigned int );
+__APIENTRY__	size_t		__APICALL__ dnnTensorPitch( uint64_t );
+__APIENTRY__	dnnStatus_t	__APICALL__ dnnCreateTensor( void**, uint64_t );
 
-__APIENTRY__	dnnStatus_t		__APICALL__ dnnCreateConvOp( dnnConvOp*, int, uint64, uint64, int, int );
-__APIENTRY__	dnnStatus_t		__APICALL__ dnnCreateFFTConvOp( dnnFFTConvOp*, size_t*, int, unsigned int, uint64, uint64 );
-__APIENTRY__	dnnStatus_t		__APICALL__ dnnCreateCellConvOp( dnnCellConvOp*, size_t*, int, unsigned int, uint64, uint64 );
-__APIENTRY__	dnnStatus_t		__APICALL__ dnnCreatePoolingOp( dnnPoolingOp*, int, unsigned int, uint64, int, int );
-__APIENTRY__	dnnStatus_t		__APICALL__ dnnCreateBiasOp( dnnBiasOp*, int, unsigned int, uint64 );
-__APIENTRY__	dnnStatus_t		__APICALL__ dnnCreateActivationOp( dnnActivationOp*, int, unsigned int );
-__APIENTRY__	dnnStatus_t		__APICALL__ dnnCreateReductionOp( dnnReductionOp*, int, int, int, unsigned int );
+__APIENTRY__	dnnStatus_t	__APICALL__ dnnCreateConvOp( dnnConvOp*, int, uint64_t, uint64_t, int, int );
+__APIENTRY__	dnnStatus_t	__APICALL__ dnnCreateFFTConvOp( dnnFFTConvOp*, size_t*, int, unsigned int, uint64_t, uint64_t );
+__APIENTRY__	dnnStatus_t	__APICALL__ dnnCreateCellConvOp( dnnCellConvOp*, size_t*, int, unsigned int, uint64_t, uint64_t );
+__APIENTRY__	dnnStatus_t	__APICALL__ dnnCreatePoolingOp( dnnPoolingOp*, int, unsigned int, uint64_t, int, int );
+__APIENTRY__	dnnStatus_t	__APICALL__ dnnCreateBiasOp( dnnBiasOp*, int, unsigned int, uint64_t );
+__APIENTRY__	dnnStatus_t	__APICALL__ dnnCreateActivationOp( dnnActivationOp*, int, unsigned int );
 
-__APIENTRY__	void			__APICALL__ dnnConv( dnnConvOp, void*, const void*, const void*, float, int, CUstream );
-__APIENTRY__	void			__APICALL__ dnnFFTConv( dnnFFTConvOp, void*, void*, const void*, const void*, float, CUstream );
-__APIENTRY__	void			__APICALL__ dnnCellConv( dnnCellConvOp, void*, void*, const void*, const void*, float, CUstream );
-__APIENTRY__	void			__APICALL__ dnnPooling( dnnPoolingOp, void*, const void*, int, CUstream );
-__APIENTRY__	void			__APICALL__ dnnUpdateBias( dnnBiasOp, void*, const void*, float, CUstream );
-__APIENTRY__	void			__APICALL__ dnnActivate( dnnActivationOp, void*, const void*, const void*, uint64, float, float, int, CUstream );
-__APIENTRY__	void			__APICALL__ dnnReduce( dnnReductionOp, void*, const void*, CUstream );
+__APIENTRY__	dnnStatus_t	__APICALL__ dnnConv( dnnConvOp, void*, const void*, const void*, float, int, CUstream );
+__APIENTRY__	dnnStatus_t	__APICALL__ dnnFFTConv( dnnFFTConvOp, void*, void*, const void*, const void*, float, CUstream );
+__APIENTRY__	dnnStatus_t	__APICALL__ dnnCellConv( dnnCellConvOp, void*, void*, const void*, const void*, float, CUstream );
+__APIENTRY__	dnnStatus_t	__APICALL__ dnnPooling( dnnPoolingOp, void*, const void*, int, CUstream );
+__APIENTRY__	dnnStatus_t	__APICALL__ dnnUpdateBias( dnnBiasOp, void*, const void*, float, CUstream );
+__APIENTRY__	dnnStatus_t	__APICALL__ dnnActivateFP( dnnActivationOp, void*, void*, const void*, uint64_t, float, CUstream );
+__APIENTRY__	dnnStatus_t	__APICALL__ dnnActivateBP( dnnActivationOp, void*, const void*, const void*, uint64_t, float, CUstream );
 
-__APIENTRY__	void			__APICALL__ dnnDestroyConvOp( dnnConvOp );
-__APIENTRY__	void			__APICALL__ dnnDestroyFFTConvOp( dnnFFTConvOp );
-__APIENTRY__	void			__APICALL__ dnnDestroyCellConvOp( dnnCellConvOp );
-__APIENTRY__	void			__APICALL__ dnnDestroyPoolingOp( dnnPoolingOp );
-__APIENTRY__	void			__APICALL__ dnnDestroyBiasOp( dnnBiasOp );
-__APIENTRY__	void			__APICALL__ dnnDestroyActivationOp( dnnActivationOp );
-__APIENTRY__	void			__APICALL__ dnnDestroyReductionOp( dnnReductionOp );
-__APIENTRY__	void			__APICALL__ dnnExit();
+__APIENTRY__	dnnStatus_t	__APICALL__ dnnDestroyConvOp( dnnConvOp );
+__APIENTRY__	dnnStatus_t	__APICALL__ dnnDestroyFFTConvOp( dnnFFTConvOp );
+__APIENTRY__	dnnStatus_t	__APICALL__ dnnDestroyCellConvOp( dnnCellConvOp );
+__APIENTRY__	dnnStatus_t	__APICALL__ dnnDestroyPoolingOp( dnnPoolingOp );
+__APIENTRY__	dnnStatus_t	__APICALL__ dnnDestroyBiasOp( dnnBiasOp );
+__APIENTRY__	dnnStatus_t	__APICALL__ dnnDestroyActivationOp( dnnActivationOp );
+__APIENTRY__	dnnStatus_t	__APICALL__ dnnExit();
 
 #ifdef __cplusplus
 }
