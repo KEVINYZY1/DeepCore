aad971e7f5d68903f4c83402f5e696fab47aebf0
XiuYuLi <295553381@qq.com>
2016-09-30 17:11:03 +0800

    Update cuda_platform.h

--- DeepCore/include/cuda/cuda_platform.h
+++ DeepCore/include/cuda/cuda_platform.h
@@ -8,13 +8,13 @@
 
 typedef struct cuda_platform{	
 	CUdevice	devices		[MAX_DEVICES_PER_NODE];
-	int			clock_rate	[MAX_DEVICES_PER_NODE];
-	int			nSM			[MAX_DEVICES_PER_NODE];
-	int			sarch		[MAX_DEVICES_PER_NODE];
-	int			slist		[MAX_DEVICES_PER_NODE+1];
-	int			n_devices;
-	int			n_sdevices;	
-	int			opt_sdev_id;
+	int		clock_rate	[MAX_DEVICES_PER_NODE];
+	int		nSM		[MAX_DEVICES_PER_NODE];
+	int		sarch		[MAX_DEVICES_PER_NODE];
+	int		slist		[MAX_DEVICES_PER_NODE+1];
+	int		n_devices;
+	int		n_sdevices;	
+	int		opt_sdev_id;
 } cuda_platform_t;
 
 int cuda_platform_init( cuda_platform_t* const );
