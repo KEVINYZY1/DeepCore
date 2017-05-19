b7f9362d37453274a641c0d240fc4480c1b80b45
XiuYuLi <295553381@qq.com>
2017-05-19 15:11:04 +0800

    update
    free some bugs and the idc_conv* operation slight faster. please read
    the "readme.txt"

--- DeepCore/include/DeepCore.h
+++ DeepCore/include/DeepCore.h
@@ -20,21 +20,19 @@
 #include<stdint.h>
 #include<cuda.h>
 
-#define dcMaskDirectionForward		    0x00000000
+#define dcMaskDirectionForward          0x00000000
 #define dcMaskDirectionBackward         0x00000001
 
 #define dcMaskPrecisionFloat            0x00000000
 #define dcMaskPrecisionHalf             0x00000002
 #define dcMaskPrecisionMixed            0x00000004
 
-#define dcMaskConvAddBias               0x00000008
-#define dcMaskConvMulDiff               0x00000008
-#define dcMaskConvBatchNormalization	0x00000010
+#define dcMaskConvAddBiasOrMulDiff      0x00000008
 
 #define	dcMaskActivationRelu            0x01000000
-#define	dcMaskActivationElu             0x02000000
-#define	dcMaskActivationTanh            0x03000000
-#define	dcMaskActivationSigm            0x04000000
+
+#define dcMaskPoolingAvg                0x00000000
+#define dcMaskPoolingMax                0x00000008
 
 #ifdef __cplusplus
 extern "C"
@@ -44,6 +42,7 @@ extern "C"
 typedef struct __dc_convOp      *  dc_convOp;
 typedef struct __dc_fftconvOp	*  dc_fftconvOp;
 typedef struct __dc_cellconvOp	*  dc_cellconvOp;
+typedef struct __dc_poolingOp   *  dc_poolingOp;
 
 typedef enum dc_status{
 	dc_success=0                 ,
@@ -52,7 +51,8 @@ typedef enum dc_status{
 	dc_error_out_of_range        ,
 	dc_error_out_of_maxsize      ,
 	dc_error_out_of_memory       ,
-	dc_error_out_of_device_memory
+	dc_error_out_of_device_memory,
+	dc_error_mutually_exclusive  ,
 } dc_status_t;
 
 DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_init();
@@ -63,25 +63,33 @@ DEEPCOREAPIENTRY int         DEEPCOREAPICALL dc_get_superdevice_device_id( int, 
 DEEPCOREAPIENTRY int         DEEPCOREAPICALL dc_get_optimal_superdevice_id();
 DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_set_device( int );
 
-DEEPCOREAPIENTRY uint64_t    DEEPCOREAPICALL dc_create_tensor_shape( uint32_t, uint32_t, uint32_t );
-DEEPCOREAPIENTRY uint64_t    DEEPCOREAPICALL dc_create_tensor_shape_filter( uint32_t, uint32_t, uint32_t );
-DEEPCOREAPIENTRY uint64_t    DEEPCOREAPICALL dc_create_tensor_shape_bias( uint32_t, uint32_t );
-DEEPCOREAPIENTRY uint64_t    DEEPCOREAPICALL dc_create_tensor_shape_fc( uint32_t, uint32_t, uint32_t );
+DEEPCOREAPIENTRY uint64_t    DEEPCOREAPICALL dc_create_tensor_shape( int, uint32_t, uint32_t );
+DEEPCOREAPIENTRY uint64_t    DEEPCOREAPICALL dc_create_tensor_shape_filter( int, uint32_t, uint32_t );
+DEEPCOREAPIENTRY uint64_t    DEEPCOREAPICALL dc_create_tensor_shape_bias( int, int );
+DEEPCOREAPIENTRY uint64_t    DEEPCOREAPICALL dc_create_tensor_shape_fc( int, uint32_t, uint32_t );
 DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_create_tensor( void**, uint64_t );
 
 DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_create_convOp( dc_convOp*, int, uint32_t, uint64_t, uint64_t, uint32_t, uint32_t );
 DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_create_fftconvOp( dc_fftconvOp*, size_t*, int, uint32_t, uint64_t, uint64_t, uint32_t );
+DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_create_fftconvOp_grad( dc_fftconvOp*, size_t*, int, uint32_t, uint64_t, uint64_t, uint32_t );
 DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_create_cellconvOp( dc_cellconvOp*, size_t*, int, uint32_t, uint64_t, uint64_t, uint32_t );
+DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_create_cellconvOp_grad( dc_cellconvOp*, size_t*, int, uint32_t, uint64_t, uint64_t, uint32_t );
+DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_create_poolingOp( dc_poolingOp*, int, uint32_t, uint64_t, uint32_t, uint32_t );
 
-DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_conv( dc_convOp, void*, const void*, const void*, const void*, const float*, CUstream );
-DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_fftconv( dc_fftconvOp, void*, void*, const void*, const void*, const void*, const float*, CUstream );
+DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_conv( dc_convOp, void*, const void*, const void*, const void*, float, CUstream );
+DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_conv_relu( dc_convOp, void*, const void*, const void*, const void*, float, float, CUstream );
+DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_fftconv( dc_fftconvOp, void*, void*, const void*, const void*, const void*, float, CUstream );
+DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_fftconv_relu( dc_fftconvOp, void*, void*, const void*, const void*, const void*, float, float, CUstream );
 DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_fftconv_grad( dc_fftconvOp, void*, void*, const void*, const void*, float, CUstream );
-DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_cellconv( dc_cellconvOp, void*, void*, const void*, const void*, const void*, const float*, CUstream );
+DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_cellconv( dc_cellconvOp, void*, void*, const void*, const void*, const void*, float, CUstream );
+DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_cellconv_relu( dc_cellconvOp, void*, void*, const void*, const void*, const void*, float, float, CUstream );
 DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_cellconv_grad( dc_cellconvOp, void*, void*, const void*, const void*, float, CUstream );
-				 			 
+DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_pooling( dc_poolingOp, void*, const void*, CUstream );
+
 DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_destroy_convOp( dc_convOp );
 DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_destroy_fftconvOp( dc_fftconvOp );
 DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_destroy_cellconvOp( dc_cellconvOp );
+DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_destroy_poolingOp( dc_poolingOp );
 DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_exit();
 
 #ifdef __cplusplus
--- DeepCore/readme.txt
+++ DeepCore/readme.txt
@@ -1,8 +1,6 @@
-﻿DeepCore_v0.7
+﻿DeepCore_v0.8
 
-支持的硬件：计算能力为3.5,3.7,5.0,5.2,6.0,6.1的NVIDIA GPU(体验版期间只支持5.2和6.1)。
-
-测试版仅支持单精度和混合精度，fp16的支持已完成约75%，不久后即可与大家见面。
+支持的硬件：计算能力为5.0,5.2,6.0,6.1的NVIDIA GPU。
 
 DeepCore目前只支持一种数据格式：CNHW
     普通数据的布局为Channel_num*Batch_size*Height*Width
@@ -10,8 +8,6 @@ DeepCore目前只支持一种数据格式：CNHW
 
 目前channel_num必须是16的倍数,对于fftconv和cellconv,filter_size_x必须等于filter_size_y且必须>1
 
-DeepCore目前仅支持正方形图像数据，也就是width=height (实际上内核层面没有这个限制，目前仅仅为了封装简单，后续会取消该限制）。
-
-目前仅支持Relu内置激活函数。
+目前仅支持Relu内置激活函数
 
 所有的测试均是和最新版本的cudnn6.0在GTX1080上做的对比，后续还会有更多的测试数据和更进一步的完善优化。
