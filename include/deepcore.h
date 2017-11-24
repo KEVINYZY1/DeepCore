dba6a7955eed63399dac9afca7db9d92e32a6c32
XiuYuLi <295553381@qq.com>
2017-11-24 15:43:32 +0800

    Update2018

--- include/deepcore.h
+++ include/deepcore.h
@@ -20,25 +20,23 @@
 #include<stdint.h>
 #include<cuda.h>
 
-#define dcMaskDirectionForward      0x00000000
-#define dcMaskDirectionBackward     0x00000001
+#define dcMaskDirectionForward  0x00000000
+#define dcMaskDirectionBackward 0x00000001
 
-#define dcMaskPrecisionFloat        0x00000000
-#define dcMaskPrecisionHalf         0x00000002
-#define dcMaskPrecisionMixed        0x00000004
+#define dcMaskPrecisionFloat    0x00000000
+#define dcMaskPrecisionHalf     0x00000002
+#define dcMaskPrecisionMixed    0x00000004
 
-#define dcMaskAddBiasOrMulDiff      0x00000008
+#define dcMaskAddBias           0x00000008
+#define dcMaskMulDrv            0x00000008
 
-#define	dcMaskActivationRelu        0x01000000
+#define	dcMaskActivationRelu    0x01000000
 
-#define dcMaskPoolingAvg            0x00000000
-#define dcMaskPoolingMax            0x00000008
-
-#define dcMaskReduceAdd             0x00000000
-#define dcMaskReduceMax             0x00000008
-#define dcMaskReduceMin             0x00000010
-#define dcMaskReduceDpxx            0x00000018
-#define dcMaskReduceDpxy            0x00000020
+#define dcMaskReduceAdd         0x00000000
+#define dcMaskReduceMax         0x00000008
+#define dcMaskReduceMin         0x00000010
+#define dcMaskReduceDpxx        0x00000020
+#define dcMaskReduceDpxy        0x00000030
 
 #ifdef __cplusplus
 extern "C"
@@ -46,8 +44,9 @@ extern "C"
 #endif
 
 typedef struct __dc_convOp      *  dc_convOp;
-typedef struct __dc_fftconvOp	*  dc_fftconvOp;
-typedef struct __dc_cellconvOp	*  dc_cellconvOp;
+typedef struct __dc_fftconvOp   *  dc_fftconvOp;
+typedef struct __dc_cellconvOp  *  dc_cellconvOp;
+typedef struct __dc_depconvOp   *  dc_depconvOp;
 typedef struct __dc_gemmOp      *  dc_gemmOp;
 typedef struct __dc_reduceOp    *  dc_reduceOp;
 typedef struct __dc_bnormOp     *  dc_bnormOp;
@@ -62,7 +61,8 @@ typedef enum dc_status{
 	dc_error_out_of_device_memory,
 	dc_error_out_of_range        ,
 	dc_error_mutually_exclusive  ,
-	dc_error_mismatch
+	dc_error_mismatch            ,
+	dc_error_unsupported
 } dc_status_t;
 
 DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_init();
@@ -83,27 +83,23 @@ DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_tensor_zero( void*, uint64_t, CU
 DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_tensor_copy( void*, uint64_t, const void*, uint64_t, size_t, size_t, CUstream );
 DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_tensor_store( void*, uint64_t, const void*, size_t, size_t, size_t, CUstream );
 DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_tensor_load( void*, size_t, const void*, uint64_t, size_t, size_t, CUstream );
-
-DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_create_convOp( dc_convOp*, int, uint32_t, uint64_t, uint64_t, uint64_t, uint32_t, uint32_t );
+DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_create_convOp( dc_convOp*, uint32_t, int, uint64_t, uint64_t, uint64_t, uint32_t );
 DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_create_fftconvOp( dc_fftconvOp*, size_t*, uint32_t, uint64_t, uint64_t, uint64_t, uint32_t );
-DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_create_fftconvOp_grad( dc_fftconvOp*, size_t*, uint32_t, uint64_t, uint64_t, uint64_t, uint32_t );
+DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_create_fftconvOp_grad( dc_fftconvOp*, size_t*, uint32_t, uint64_t, uint64_t, uint64_t );
 DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_create_cellconvOp( dc_cellconvOp*, size_t*, uint32_t, uint64_t, uint64_t, uint64_t, uint32_t );
-DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_create_cellconvOp_grad( dc_cellconvOp*, size_t*, uint32_t, uint64_t, uint64_t, uint64_t, uint32_t );
-DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_create_gemmOp( dc_gemmOp*, int, uint32_t, uint64_t, uint64_t, uint64_t );
+DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_create_cellconvOp_grad( dc_cellconvOp*, size_t*, uint32_t, uint64_t, uint64_t, uint64_t );
+DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_create_gemmOp( dc_gemmOp*, uint32_t, int, uint64_t, uint64_t, uint64_t );
+DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_create_gemmOp_grad( dc_gemmOp*, uint32_t, int, uint64_t, uint64_t, uint64_t );
 DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_create_reduceOp( dc_reduceOp*, uint32_t, uint64_t );
 DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_create_bnormOp( dc_bnormOp*, uint32_t, uint64_t, uint64_t );
 
 DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_conv( dc_convOp, void*, const void*, const void*, const void*, float, CUstream );
-DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_conv_relu( dc_convOp, void*, const void*, const void*, const void*, float, float, CUstream );
 DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_fftconv( dc_fftconvOp, void*, void*, const void*, const void*, const void*, float, CUstream );
-DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_fftconv_relu( dc_fftconvOp, void*, void*, const void*, const void*, const void*, float, float, CUstream );
-DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_fftconv_grad( dc_fftconvOp, void*, void*, const void*, const void*, float, CUstream );
+DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_fftconv_grad( dc_fftconvOp, void*, void*, const void*, const void*, CUstream );
 DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_cellconv( dc_cellconvOp, void*, void*, const void*, const void*, const void*, float, CUstream );
-DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_cellconv_relu( dc_cellconvOp, void*, void*, const void*, const void*, const void*, float, float, CUstream );
-DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_cellconv_grad( dc_cellconvOp, void*, void*, const void*, const void*, float, CUstream );
+DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_cellconv_grad( dc_cellconvOp, void*, void*, const void*, const void*, CUstream );
 DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_gemm( dc_gemmOp, void*, const void*, const void*, const void*, float, CUstream );
-DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_gemm_relu( dc_gemmOp, void*, const void*, const void*, const void*, float, float, CUstream );
-DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_gemm_grad( dc_gemmOp, void*, const void*, const void*, float, CUstream );
+DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_gemm_grad( dc_gemmOp, void*, const void*, const void*, CUstream );
 DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_reduce( dc_reduceOp, void*, const void*, const void*, float, CUstream );
 DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_bnorm( dc_bnormOp, void*, void*, void*, const void*, const void*, const void*, CUstream );
 DEEPCOREAPIENTRY dc_status_t DEEPCOREAPICALL dc_bnorm_grad( dc_bnormOp, void*, void*, void*, const void*, const void*, const void*, const void*, const void*, CUstream );
