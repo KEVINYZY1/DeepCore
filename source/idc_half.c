f41f7d1179a712e5a46041fc360c9f7b51bc341d
XiuYuLi <295553381@qq.com>
2017-11-24 07:26:46 +0000

    Delete idc_half.c

--- source/idc_half.c
+++
@@ -1,7 +0,0 @@
-﻿#include"../include/idc_half.h"
-
-unsigned short idc_float2half( float f )
-{
-    uint32_t c=*((uint32_t*)&f);
-    return (((c>>16)&0x8000)|((((c&0x7f800000)-0x38000000)>>13)&0x7c00)|((c>>13)&0x03ff));
-}
